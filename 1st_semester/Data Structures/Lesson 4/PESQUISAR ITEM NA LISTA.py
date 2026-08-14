d95b586406b9960e81ef3657729aeb29d322486e
Abdiel Paulino <164289941+abdielpaulino@users.noreply.github.com>
2026-08-14 19:17:32 -0300

    Delete 1st_semester/Data Structures directory

--- 1st_semester/Data Structures/Lesson 4/PESQUISAR ITEM NA LISTA.py
+++
@@ -1,9 +0,0 @@
-lista=[1,2,6,8,3,9,90,14,500]
-print(lista)
-
-elemento=int(input("Digite o elemento que deseja verificar: "))
-
-if elemento in lista:
-    print("Elemento encontrado.")
-else:
-    print("Elemento não encontrado.")
